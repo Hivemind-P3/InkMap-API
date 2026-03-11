af8d5c3bcb4bb9f21360a1169b2e2aec4cf55866
Axel <ajimenezq1@ucenfotec.ac.cr>
2026-03-11 16:58:33 -0600

    Commit inicial

---
+++ src/main/java/com/cenfotec/inkmapapi/InkMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.inkmapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkMapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cenfotec/inkmapapi/InkMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.inkmapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkMapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
