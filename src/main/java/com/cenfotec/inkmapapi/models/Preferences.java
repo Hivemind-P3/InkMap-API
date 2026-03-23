169762e99eaeb3a17e636cf168d1cabef5e49bb8
Axel <ajimenezq1@ucenfotec.ac.cr>
2026-03-23 02:08:53 -0600

    Agregar entidad Preferences

---
+++ src/main/java/com/cenfotec/inkmapapi/models/Preferences.java
@@ -0,0 +1,40 @@
+package com.cenfotec.inkmapapi.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+/**
+ * Entiddad que representa las preferencias del usuario
+ */
+@Entity
+@Table(name = "preferencias")
+@Getter
+@Setter
+public class Preferences {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_preferencias")
+    private Long id;
+
+    private boolean notificacionesCorreo;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToOne
+    @JoinColumn(name = "id_codigoColor")
+    private CodigoColor codigoColor;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private User user;
+}
