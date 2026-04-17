f84bd1105e6cb07cdb60f32a43958c99a15c6537
jcarv-fis <jcarvajalm@ucenfotec.ac.cr>
2026-04-17 15:09:54 -0600

    se corrige bug en búsqueda de wiki por título

--- src/main/java/com/cenfotec/inkmapapi/repository/WikiRepository.java
+++ src/main/java/com/cenfotec/inkmapapi/repository/WikiRepository.java
@@ -5,6 +5,8 @@ import com.cenfotec.inkmapapi.models.Wiki;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,8 +14,9 @@ public interface WikiRepository extends JpaRepository<Wiki, Long> {
 
     Page<Wiki> findByProjectOrderByCreatedAtDesc(Project project, Pageable pageable);
 
+    @Query("SELECT w FROM Wiki w WHERE w.project = :project AND LOWER(w.title) LIKE LOWER(CONCAT('%', :title, '%')) ORDER BY w.createdAt DESC")
     Page<Wiki> findByProjectAndTitleContainingIgnoreCaseOrderByCreatedAtDesc(
-            Project project, String title, Pageable pageable);
+            @Param("project") Project project, @Param("title") String title, Pageable pageable);
 
     Optional<Wiki> findByIdAndProject(Long id, Project project);
 }
